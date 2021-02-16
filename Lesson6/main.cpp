8a436bc756d46d278e821cddbfa1eb1b32bac8db
Maxim <maxim_sdrv@mail.ru>
2021-02-16 21:06:32 +0300

    added homework Task 1 for lesson6

--- Lesson6/main.cpp
+++ Lesson6/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+void ConnectFiles(const string &f1, const string &f2){
+
+}
+
 int main() {
+/* Task 1. Написать программу, которая создаст два
+ * текстовых файла, примерно по 50-100 символов в каждом
+ * (особого значения не имеет); */
+
+    string words1;
+    string words2;
+    ifstream file_in_words1;
+    ifstream file_in_words2;
+    file_in_words1.open("words1.txt");
+    file_in_words2.open("words2.txt");
+    if (file_in_words1.is_open() && file_in_words2.is_open()){
+        getline(file_in_words1, words1);
+        getline(file_in_words2, words2);
+    }
+
+    ofstream file1, file2;
+    file1.open("file1.txt");
+    file2.open("file2.txt");
+    if (file1.is_open() && file2.is_open()){
+        file1 << words1;
+        file2 << words2;
+    }
+
+/* Task 2.   Написать функцию, «склеивающую» эти файлы, предварительно
+ * буферизуя их содержимое в динамически выделенный сегмент памяти нужного размера. */
+
+
+
     return 0;
 }
